8a6044c7bfbcefa353edaf3e457d324c232aa944
Fredrick <freddyfred976@gmail.com>
2023-06-23 12:11:17 +0300

    complet

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(freeride){
   // Write your code here!
+  if (freeride <= 2000) {
+    return "This one is on me!"
+  } else if (freeride >= 2001 && freeride < 2501){
+    return 'I will gladly take your thirty bucks.'
+  } else if(freeride > 2500);{
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return(city == 'NYC' ? "Ok, sounds good." : "No go.")
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generous){
   // Write your code here!
+  switch(generous){
+    case 'generous':
+      return 'Thank you so much.'
+      case 'not as generous':
+      return 'Thank you.'
+      case 'thanks for everything':
+      return 'Bye.'
+  }
 }
